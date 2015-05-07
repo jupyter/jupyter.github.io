967b65914438465a376814b5160dbe87100e88df
Nicholas Bollweg <nick.bollweg@gmail.com>
2015-05-06 23:56:44 -0400

    feedback from #7

--- scripts/kernels.js
+++ scripts/kernels.js
@@ -35,6 +35,8 @@
   d3.json("./kernels.json", kernelsLoaded);
 
   updateUI();
+  
+  d3.select(window).on("resize", update);
 
 
   function updateUI(){
@@ -66,18 +68,24 @@
 
           updateUI();
           update();
-        })
+        });
     });
 
-    sort.select("i")
+    sort
       .classed({
-        "fa-arrow-up": function(d){
-          return d.value === _sort && _sortDir === "ascending";
-        },
-        "fa-arrow-down": function(d){
-          return d.value === _sort && _sortDir === "descending";
-        }
+        "btn-primary": isActiveSort()
       })
+      .select("i")
+      .classed({
+        "fa-arrow-up": isActiveSort("ascending"),
+        "fa-arrow-down": isActiveSort("descending")
+      });
+  }
+  
+  function isActiveSort(direction){
+    return function(d){
+      return d.value === _sort && (!direction || _sortDir === direction);
+    };
   }
 
   function kernelsLoaded(err, data){
@@ -108,20 +116,21 @@
 
 
   function updateFeature(selection){
-    selection
+    var feature = selection
       .enter()
       .append("span")
       .classed({feature: 1})
-      .append("i")
+      .attr({title: function(d){ return _features[d.key || d].description; }})
+      .call(tooltip);
+    
+    feature.append("i")
       .attr({
         "class": function(d){
           return _features[d.key || d].icon.replace(":", "-");
         },
         title: function(d){ return _features[d.key || d].name; }
         })
-      .classed({fa: 1, "fa-fw": 1})
-      .attr({title: function(d){ return _features[d.key || d].description; }})
-      .call(tooltip);
+      .classed({fa: 1, "fa-fw": 1});
   }
 
   function sortKernels(a, b){
@@ -132,46 +141,66 @@
 
   function update(){
     var feature = d3.select(".features")
+      .classed({"btn-group": 1, "btn-group-justified": 1})
       .selectAll(".feature")
       .data(d3.entries(_features))
       .call(updateFeature)
       .classed({
-        "text-primary": function(d){ return d.value.filtered; },
-        "text-muted": function(d){ return !d.value.filtered; },
-        "col-md-3": 1
+        btn: 1,
+        "btn-primary": function(d){ return d.value.filtered; }
       })
       .on("click", function(d){
         d.value.filtered = !d.value.filtered;
         updateUI();
         update();
-      })
+      });
     
-    feature.select("i").classed({"fa-2x": 1})
-    
-    feature.selectAll("label")
+    feature.selectAll("span")
       .data(function(d){ return [d]; })
       .enter()
-      .append("label")
+      .append("span")
       .text(function(d){ return d.value.name; });
 
-    var kernelData = d3.entries(_kernels)
+    var kernelData = stackKernels(d3.entries(_kernels)
       .filter(filterKernels())
-      .sort(sortKernels);
+      .sort(sortKernels));
 
-    var kernel = d3.select(".kernels")
-      .classed({row: 1})
+    var kernels = d3.select(".kernels");
+
+    var column = kernels.selectAll(".kernel-col")
+      .data(kernelData)
+    
+    column.exit().remove();
+    column.enter().append("div");
+    column.attr("class", function(d){
+      return "kernel-col col-md-" + (12 / kernelData.length);
+    });
+    
+    var kernel = column
       .selectAll(".kernel")
-      .data(kernelData, function(d){ return d.key; });
+      .data(Object, function(d){ return d.key; });
 
     kernel.exit().remove();
 
-    kernel.enter()
-      .append("div")
-      .classed({kernel: 1, "col-md-4": 1})
+    kernel.enter().append("div")
+      .classed({kernel: 1})
       .call(updateKernel);
 
     kernel.order();
   }
+  
+  function stackKernels(kernels){
+    var width = d3.select("body").node().clientWidth,
+      columnCount = width < 990 ? 1 : 3,
+      columnClasses = "kernel-col col-md-" + (12 / columnCount);
+    
+    return d3.range(columnCount).map(function(undef, col){
+      return kernels.filter(function(kernel, ki){
+        return ki % columnCount === col;
+      });
+    });
+  }
+
 
   function filterKernels(){
     var searchBits = _search.toLowerCase().split(" "),
@@ -195,12 +224,12 @@
             (searchLanguages(bit, d.value.languages));
         }, 0);
       return searchHit && featureHit;
-    }
+    };
   }
 
   function searchLanguages(q, languages){
     return d3.entries(languages).reduce(function(hit, d){
-      return hit + (_languages[d.key].name.toLowerCase().indexOf(q) !== -1)
+      return hit + (_languages[d.key].name.toLowerCase().indexOf(q) !== -1);
     }, 0);
   }
   
@@ -244,7 +273,7 @@
   }
   
   function tooltip(selection){
-    selection.each(function(){ $(this).tooltip(); });
+    selection.each(function(){ $(this).tooltip({container: "body"}); });
   }
 
   function updateKernel(kernel){
@@ -267,11 +296,14 @@
       .text(function(d){ return d.value.name; })
       .attr({href: function(d){ return d.value.url; }});
 
-    title.append("span")
+    title
+      .filter(function(d){ return d.value.version; })
+      .append("span")
       .classed({version: 1, "text-muted": 1, "text-sm": 1})
-      .text(function(d){ return d.value.version; });
+      .text(function(d){ return "v" + d.value.version; });
 
-    var lang = body.append("div")
+    var lang = body.filter(function(d){ return d.value.languages; })
+      .append("div")
       .selectAll(".language")
       .data(function(d){ return d3.entries(d.value.languages); })
       .enter()
@@ -282,13 +314,13 @@
       .text(function(d){
         return _languages[d.key].name;
       })
-      .attr({href: function(d){ return  _languages[d.key].url; }})
+      .attr({href: function(d){ return  _languages[d.key].url; }});
 
     lang.selectAll("label")
       .data(function(d){ return d.value.versions; })
       .enter()
       .append("label")
-      .classed({label: 1, "label-info": 1})
+      .classed({label: 1, "label-default": 1})
       .text(String);
 
     updateFeatures(body)
