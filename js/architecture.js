ff04dbb15bfad36d8e5a45c918092da471fd4f29
cameronoelsen <cameronoelsen@gmail.com>
2015-08-26 16:44:37 -0700

    Architecture Buttons - Complete

---
+++ js/architecture.js
@@ -0,0 +1,15 @@
+$(function() {
+	$(".bt").click(function() {
+		$(".front").toggleClass("flipped");
+	});
+});
+$(function() {
+	$(".bt1").click(function() {
+		$(".front2").toggleClass("flipped");
+	});
+});
+$(function() {
+	$(".bt2").click(function() {
+		$(".front3").toggleClass("flipped");
+	});
+});
