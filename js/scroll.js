a04aa478bcb950fe27b6478022baa90f41ce133f
cameronoelsen <cameronoelsen@gmail.com>
2017-04-27 11:24:39 -0700

    Made final style refinements to documentation page, added links to all of the documentation items, linked the main website to the documentation page

--- js/scroll.js
+++ js/scroll.js
@@ -18,15 +18,15 @@ $(document).ready(function() {
                 $('.navbar').addClass('jupytercon-navbar');
                 $('.tab').removeClass('black-tab');
                 document.getElementById('jupyter-nav-logo').src = '/assets/white_nav_logo.svg';
-                $('.icon-bar').addClass('white-icon-bar');   
+                $('.icon-bar').addClass('white-icon-bar');
             }
-        }  
+        }
     });
     $(window).on("scroll", function () {
         if ($(this).scrollTop() > 0) {
             $(".navbar").addClass("navbar-scroll");
             // $(".navbar").stop().animate({opacity: 0});
-        
+
             // For JupyterCon Custom Navbar
             if (window.location.pathname == '/' || window.location.pathname == '/index.html') {
                 $('.navbar').removeClass('jupytercon-navbar');
@@ -38,7 +38,7 @@ $(document).ready(function() {
         else {
             $(".navbar").removeClass("navbar-scroll");
             // $(".navbar").stop().animate({opacity: 1});
-        
+
             // For JupyterCon Custom Navbar
             if (window.location.pathname == '/' || window.location.pathname == '/index.html') {
                 if (window.outerWidth < 768) {
@@ -49,9 +49,9 @@ $(document).ready(function() {
                     $('.navbar').addClass('jupytercon-navbar');
                     $('.tab').removeClass('black-tab');
                     document.getElementById('jupyter-nav-logo').src = '/assets/white_nav_logo.svg';
-                    $('.icon-bar').addClass('white-icon-bar');   
+                    $('.icon-bar').addClass('white-icon-bar');
                 }
             }
         }
-    }); 
+    });
 });
