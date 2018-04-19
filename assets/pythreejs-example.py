00a47b94af8271ec9d42841cc727b50a1c15fb21
Vidar Tonaas Fauske <vidartf@gmail.com>
2018-04-19 11:49:10 +0200

    Update Widgets - pythreejs example to latest relase

--- assets/pythreejs-example.py
+++ assets/pythreejs-example.py
@@ -5,21 +5,21 @@ function f(origu,origv) {
     // scale u and v to the ranges I want: [0, 2*pi]
     var u = 2*Math.PI*origu;
     var v = 2*Math.PI*origv;
-    
+
     var x = Math.sin(u);
     var y = Math.cos(v);
     var z = Math.cos(u+v);
-    
-    return new THREE.Vector3(x,y,z)
+
+    return new THREE.Vector3(x,y,z);
 }
 """
 
-surf_g = ParametricGeometry(func=f);
-surf = Mesh(geometry=surf_g, material=LambertMaterial(color='green', side='FrontSide'))
-surf2 = Mesh(geometry=surf_g, material=LambertMaterial(color='yellow', side='BackSide'))
-scene = Scene(children=[surf, surf2, AmbientLight(color='#777777')])
-c = PerspectiveCamera(position=[2.5, 2.5, 2.5], up=[0, 0, 1],
+surf_g = ParametricGeometry(func=f, slices=16, stacks=16)
+surf = Mesh(geometry=surf_g, material=MeshLambertMaterial(color='green', side='FrontSide'))
+surf2 = Mesh(geometry=surf_g, material=MeshLambertMaterial(color='yellow', side='BackSide'))
+c = PerspectiveCamera(position=[5, 5, 3], up=[0, 0, 1],
                       children=[DirectionalLight(color='white',
                                                  position=[3, 5, 1],
                                                  intensity=0.6)])
-Renderer(camera=c, scene=scene, controls=[OrbitControls(controlling=c)])
+scene = Scene(children=[surf, surf2, c, AmbientLight(intensity=0.5)])
+Renderer(camera=c, scene=scene, controls=[OrbitControls(controlling=c)], width=400, height=400)
