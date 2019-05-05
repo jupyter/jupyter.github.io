d8dbc21db031f234bbd3fb2d1840eb9243bf713e
artur.trzesiok <artur.trzesiok@gmail.com>
2019-05-05 09:21:14 +0200

    colormap jet->Inferno and better description

--- assets/k3d-example.py
+++ assets/k3d-example.py
@@ -8,6 +8,6 @@ plot = k3d.plot()
 
 for l, a in zip(lines, lines_attributes):
     plot += k3d.line(l, attribute=a, width=0.0001,
-                     color_map=k3d.paraview_color_maps.Jet, color_range=[0,0.5], shader='mesh',
+                     color_map=k3d.matplotlib_color_maps.Inferno, color_range=[0,0.5], shader='mesh',
                      compression_level=9)
 plot.display()
